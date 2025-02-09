9c62567fb9c68a93718d3f9bdd0dbdcae2ff31a4
Anton Bogomolov <ashteriel@ya.ru>
2025-02-09 21:27:54 +0200

    Add HTTP status codes for bad requests and forbidden responses; enhance registration tests

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,14 +1,29 @@
+import pytest
 from api.auth import Register
 from fixtures.delete_user import delete_user
 from utils.enums import HttpCodes
 
 def test_register_new_uniq_user(delete_user):
     reg_client = Register()
-
     response = reg_client.register_new_user
+
     assert response.status_code == HttpCodes.OK
 
     token = response.json().get("accessToken")
     headers = {"Authorization": f"{token}"}
-
     delete_user.update(headers)
+
+
+def test_register_existing_user():
+    reg_client = Register()
+    response = reg_client.register_existing_user
+
+    assert response.status_code == HttpCodes.FORBIDDEN
+
+
+@pytest.mark.parametrize("param", ["email", "password", "name"])
+def test_register_bad_payload(param):
+    reg_client = Register()
+    response = reg_client.register_user_bad_payload(param)
+
+    assert response.status_code == HttpCodes.FORBIDDEN
--- utils/enums/http_codes.py
+++ utils/enums/http_codes.py
@@ -4,6 +4,8 @@ from enum import Enum
 class HttpCodes(int, Enum):
     OK = 200
     ACCEPTED = 202
+    BAD_REQUEST = 400
+    FORBIDDEN = 403
 
     def __int__(self) -> int:
         return self.value
