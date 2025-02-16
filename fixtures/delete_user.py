4e9b75f05b838547cec89b33fab1defc070444b7
Anton Bogomolov <ashteriel@yandex.ru>
2025-02-16 16:30:01 +0200

    fix

--- fixtures/delete_user.py
+++ fixtures/delete_user.py
@@ -4,12 +4,13 @@ from utils.enums import HttpCodes
 from utils.helpers.help_func import generate_headers
 
 @pytest.fixture
-def delete_user():
+def delete_user(request, response):
 
-    def _delete_user(response):
-        token = response.json().get("accessToken")
-        headers = generate_headers(token)
-        res = User(headers=headers).delete_user
-        assert res.status_code == HttpCodes.ACCEPTED
+    token = response.json().get("accessToken")
+    headers = generate_headers(token)
+    user = User(headers=headers)
 
-    yield _delete_user
+    yield
+
+    res = user.delete_user()
+    assert res.status_code == HttpCodes.ACCEPTED
